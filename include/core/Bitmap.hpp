d2c3d728399cd5a8a58dc3d70c2cae21f11b6586
BlauHimmel <461310150@qq.com>
2018-11-02 17:15:48 +0800

    Add Bitmap.

---
+++ include/core/Bitmap.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <core\Common.hpp>
+#include <core\Color.hpp>
+#include <core\Vector.hpp>
+
+NAMESPACE_BEGIN
+
+/**
+* \brief Stores a RGB high dynamic-range bitmap
+*
+* The bitmap class provides I/O support using the OpenEXR file format
+*/
+class Bitmap : public Eigen::Array<Color3f, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+{
+public:
+	using Base = Eigen::Array<Color3f, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+	/**
+	* \brief Allocate a new bitmap of the specified size
+	*
+	* The contents will initially be undefined, so make sure
+	* to clear it if necessary.
+	*/
+	Bitmap(const Vector2i & Size = Vector2i(0, 0));
+
+	/// Load an OpenEXR file with the specified filename
+	Bitmap(const std::string & Filename);
+
+	/// Save the bitmap as an EXR file with the specified filename
+	void Save(const std::string & Filename);
+};
+
+NAMESPACE_END
