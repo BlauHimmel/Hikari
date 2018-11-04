9ef4201042581b728dcacc6a560a4f5515957573
周宇明 <461310150@qq.com>
2018-11-04 23:56:54 +0800

    Add NormalIntegrator.

--- src/integrator/NormalIntegrator.cpp
+++ src/integrator/NormalIntegrator.cpp
@@ -1,4 +1,6 @@
 #include <integrator\NormalIntegrator.hpp>
+#include <core\Mesh.hpp>
+#include <core\Scene.hpp>
 
 NAMESPACE_BEGIN
 
@@ -6,23 +8,26 @@ REGISTER_CLASS(NormalIntegrator, XML_INTEGRATOR_NORMAL);
 
 NormalIntegrator::NormalIntegrator(const PropertyList & PropList)
 {
-	m_MyProperty = PropList.GetString("myProperty");
-	LOG(INFO) << "Parameter value was : " << m_MyProperty << std::endl;
+
 }
 
 Color3f NormalIntegrator::Li(const Scene * pScene, Sampler * pSampler, const Ray3f & Ray) const
 {
-	return Color3f(0, 1, 0);
+	/* Find the surface that is visible in the requested direction */
+	Intersection Isect;
+	if (!pScene->RayIntersect(Ray, Isect))
+	{
+		return Color3f(0.0f);
+	}
+
+	/* Return the component-wise absolute value of the shading normal as a color */
+	Normal3f N = Isect.ShadingFrame.N.cwiseAbs();
+	return Color3f(N.x(), N.y(), N.z());
 }
 
 std::string NormalIntegrator::ToString() const
 {
-	return tfm::format(
-		"NormalIntegrator[\n"
-		"  myProperty = \"%s\"\n"
-		"]",
-		m_MyProperty
-	);
+	return tfm::format("NormalIntegrator[]");
 }
 
 NAMESPACE_END
