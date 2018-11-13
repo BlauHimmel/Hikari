0c8a55bc91f07a788b5d8d681fcd24c1ea5b5de7
周宇明 <461310150@qq.com>
2018-11-13 08:50:29 +0800

    Add tent and disk sampling functions.

--- src/core/Sampling.cpp
+++ src/core/Sampling.cpp
@@ -14,22 +14,47 @@ float Sampling::SquareToUniformSquarePdf(const Point2f & Pt)
 
 Point2f Sampling::SquareToTent(const Point2f & Sample)
 {
-	throw HikariException("Sampling::SquareToTent(const Point2f & Sample) is not yet implemented!");
+	return Point2f(
+		(Sample.x() < 0.5f ? (std::sqrtf(2.0f * Sample.x()) - 1.0f) : (1.0f - std::sqrtf(2.0f - 2.0f * Sample.x()))),
+		(Sample.y() < 0.5f ? (std::sqrtf(2.0f * Sample.y()) - 1.0f) : (1.0f - std::sqrtf(2.0f - 2.0f * Sample.y())))
+	);
 }
 
 float Sampling::SquareToTentPdf(const Point2f & Pt)
 {
-	throw HikariException("Sampling::SquareToTentPdf(const Point2f & Pt) is not yet implemented!");
+	return (1.0f - std::abs(Pt.x())) * (1.0f - std::abs(Pt.y()));
 }
 
 Point2f Sampling::SquareToUniformDisk(const Point2f & Sample)
 {
-	throw HikariException("Sampling::SquareToUniformDisk(const Point2f & Sample) is not yet implemented!");
+	Point2f OffsetSample = 2.0f * Sample - Point2f(1.0f);
+	if (OffsetSample.x() == 0.0f && OffsetSample.y() == 0.0f)
+	{
+		return Point2f(0.0f);
+	}
+
+	float Theta, R;
+
+	if (std::abs(OffsetSample.x()) > std::abs(OffsetSample.y()))
+	{
+		R = OffsetSample.x();
+		Theta = M_PI * 0.25f * (OffsetSample.y() / OffsetSample.x());
+	}
+	else
+	{
+		R = OffsetSample.y();
+		Theta = M_PI * 0.5f - M_PI * 0.25f * (OffsetSample.x() / OffsetSample.y());
+	}
+
+	return Point2f(
+		R * std::cosf(Theta),
+		R * std::sinf(Theta)
+	);
 }
 
 float Sampling::SquareToUniformDiskPdf(const Point2f & Pt)
 {
-	throw HikariException("Sampling::SquareToUniformDiskPdf(const Point2f & Pt) is not yet implemented!");
+	return (Pt.norm() <= 1.0f) ? 1.0f / M_PI : 0.0f;
 }
 
 Vector3f Sampling::SquareToUniformSphere(const Point2f & Sample)
